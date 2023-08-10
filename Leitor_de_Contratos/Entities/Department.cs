19728d1a75715726ed58c5d7fc54b8be01c03283
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-08-10 18:59:52 -0300

    Criação do sistema de registro de contratos

---
+++ Leitor_de_Contratos/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Leitor_de_Contratos.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Leitor_de_Contratos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Leitor_de_Contratos.Entities;
+using Leitor_de_Contratos.Entities.Enums;
+
+namespace Leitor_de_Contratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o nome do departamento: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Informe os dados do trabalhador:");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+            Console.Write("Nivel de senioridade(Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Informe a base salarial: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.WriteLine();
+            Console.Write("Quantos contratos para este trabalhador? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Entre com os dados do #{i} contrato: ");
+                Console.Write("Data (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duração (horas): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+
+            Console.WriteLine();
+            Console.Write("Entre com o mês e ano para calcular o ganho (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            Console.WriteLine("Nome: " + worker.Name);
+            Console.WriteLine("Departamento: " + worker.Department.Name);
+            Console.WriteLine("Ganhos de " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
